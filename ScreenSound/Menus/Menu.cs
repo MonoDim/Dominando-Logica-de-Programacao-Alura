2a125d95e5bc05800543732a45dbe59eb981e32f
Gabriel Ramos <sanchesramosg@gmail.com>
2025-01-09 15:22:40 -0300

    Conversa da porra
    Nao consigo me concentrar em porra nenhuma, hoje eu vou pra casa mais cedo mesmo e foda-se

---
+++ ScreenSound/Menus/Menu.cs
@@ -0,0 +1,13 @@
+﻿namespace OuvirMusga.Menus;
+
+internal class Menu
+{
+    public void ExibirTituloDaOpcao(string titulo)
+    { 
+        int quantidadeDeLetras = (titulo.Length)+2;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '=');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine($"={titulo}=");
+        Console.WriteLine(asteriscos + "\n");
+    }
+}
